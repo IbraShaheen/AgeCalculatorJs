9b887440a6e3c3c7d568d8a67645ac792810595f
IbraShaheen <engibraheem1995@gmail.com>
2021-05-19 10:05:21 +0300

    under processing

--- app.js
+++ app.js
@@ -13,17 +13,53 @@
 
 // This functions should check the integrity of the parameters and pass true/false
 function checkParamsFn(year, month, day) {
-  // Write your code here
+  if (Number.isInteger(year) && Number.isInteger(month) && Number.isInteger(day)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // This functions checks if the person is or above 18 years of age, return true/false
+
+//let y1 = new Date().getFullYear();
 function checkOverEighteenFn(year, month, day) {
-  // Write your code here
+  let y = new Date().getFullYear();
+  let m = new Date().getMonth();
+  let d = new Date().getDay();
+  if (y - year > 18) return true;
+  return false;
 }
 
+//checkOverEighteenFn(2010, 04, 12)
+/*let fn1 = checkParamsFn();
+let fn2 = checkOverEighteenFn();
 function calculateAgeFn(year, month, day) {
-  // Write your code here
+if (fn1&&fn2) {
+  let a = (`Ur age is: ${y-year}`);
+  return a;
+  let  b = ("error:") ;
+} else { return b; }
 }
+calculateAgeFn(1995,3,18);*/
+
+let fn1 = checkParamsFn();
+let fn2 = checkOverEighteenFn();
+function calculateAgeFn(year, month, day) {
+  let y = new Date().getFullYear();
+  let m = new Date().getMonth();
+  let d = new Date().getDay();
+
+  if (fn1 && fn2) {
+    let a = (y-year);
+    //let a = ("Your Age is :" + (y-year));
+    //let a = (`Your Age is : ${y-year} years,${m-month} months and ${d-day} days`);
+    return a;
+  } else {
+    return "error:";
+  }
+}
+calculateAgeFn(1995, 3, 18);
 
 // Look at the naming of the functions. it looks like salwaBaqer, where
 // the first letter of the first word is small, while the first letter of the
